b3b1880c40f1675576186316f5b050497dce2203
sidhi001919 <sidhiproject8@gmail.com>
2025-04-12 23:55:48 +0530

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -88,21 +88,21 @@ export const testimonialsData = [
     name: "Aarav Sharma",
     role: "Graphic Designer",
     stars: 5,
-    text: `I've been using imagiNATION for over a year now, especially for my freelance projects, and it's been a game changer. It's quick, easy to use, and has saved me so much time. Highly recommend it!`,
+    text: `I've been using IMAGINEx for over a year now, especially for my freelance projects, and it's been a game changer. It's quick, easy to use, and has saved me so much time. Highly recommend it!`,
   },
   {
     image: 'https://i.pinimg.com/236x/b8/d5/5a/b8d55a60bd20a95af88e8805318c5a89.jpg',
     name: "Priya Desai",
     role: "Content Creator",
     stars: 5,
-    text: `imagiNATION has made my life so much easier. As a content creator, I need tools that are both efficient and reliable, and this one ticks all the boxes. It has improved the quality of my content and cut down my editing time significantly.`,
+    text: `IMAGINEx has made my life so much easier. As a content creator, I need tools that are both efficient and reliable, and this one ticks all the boxes. It has improved the quality of my content and cut down my editing time significantly.`,
   },
   {
     image: 'https://i.pinimg.com/236x/4b/05/16/4b05160aa0e453cf15dc0c69a7769103.jpg',
     name: "Sofia",
     role: "Graphic Designer",
     stars: 5,
-    text: `I've tried several background removal tools, but bg.removal stands out. The ease of use and accuracy is unmatched. It’s the perfect tool for any designer who needs to streamline their workflow.`,
+    text: `IMAGINEx has made my life so much easier. It’s the perfect tool for any designer who needs to streamline their workflow.`,
   },
 ];
 
